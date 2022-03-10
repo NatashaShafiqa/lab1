b259783b3226b1691dffb0ca427533f8dd37d3c6
NatashaShafiqa <natashashafiqa5@gmail.com>
2022-03-10 23:22:46 +0800

    Second commit

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,13 @@
+package samples.demo;
+
+public class Location {
+	private String address;
+	
+	public String address() {
+		return address;
+	}
+	
+	public void setAddress(String newAddress) {
+		this.address = newAddress;
+	}
+}
